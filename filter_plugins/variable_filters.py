1e5455434a7d9b088b6fa965eb85c6c343c1c2fb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-17 09:55:54 -0500

    Test views

--- filter_plugins/variable_filters.py
+++ filter_plugins/variable_filters.py
@@ -1,10 +1,15 @@
+import re
+
+
 class FilterModule(object):
     def filters(self):
         return {'to_snake_case': self.to_snake_case,
                 'to_dash_case': self.to_dash_case, }
 
     def to_snake_case(self, variable_name):
-        return variable_name
+        name = re.sub(r'(?<!^)(?=[A-Z])', '_', variable_name).lower()
+        return name
 
     def to_dash_case(self, variable_name):
-        return variable_name
+        name = re.sub(r'(?<!^)(?=[A-Z])', '-', variable_name).lower()
+        return name
